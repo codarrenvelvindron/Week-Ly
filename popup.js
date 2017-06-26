ad096f42d3ca8fe560b31c74936cf2b022fb3350
Codarren Velvindron <codarren@hackers.mu>
2017-06-26 20:22:29 +0400

    Complete rework external update

--- popup.js
+++ popup.js
@@ -1,24 +1,12 @@
-//read quotes from external xml
-var UrlQuote="weeklyquote.xml";
-window.onload = function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    myFunction(this);
-    }
-  };
-  xhttp.open("GET", UrlQuote, true);
-  xhttp.send(null);
+var Url = "https://raw.githubusercontent.com/codarrenvelvindron/Week-Ly/master/weeklyquote.json"
+var xhr = new XMLHttpRequest();
+xhr.open("GET", Url, true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    var json = JSON.parse(xhr.responseText);
+	var i = Math.floor((Math.random() * 9) +1);  
+	document.getElementById("quote").innerHTML = json.quotestore.q[i].quote;
+	document.getElementById("author").innerHTML = "~~" + json.quotestore.q[i].author;
+  }
 }
-
-function myFunction(xml) {
-  	
-  var i = Math.floor((Math.random() * 9) +1);
-  var xmlDoc = xml.responseXML;
-  var x = xmlDoc.getElementsByTagName("q");
-  //window.alert(i);
-  quotevar = x[i].getElementsByTagName("quote")[0].childNodes[0].nodeValue;
-  authorvar= x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue;
-  document.getElementById("quote").innerHTML = quotevar;
-  document.getElementById("author").innerHTML = authorvar;
-}
+xhr.send();
